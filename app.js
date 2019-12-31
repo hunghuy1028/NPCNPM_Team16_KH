f25dbd7979ea16b29850f1cea5a88b6d374fc583
thkduy <56596480+thkduy@users.noreply.github.com>
2019-12-31 21:15:43 +0700

    view chi tiết phim

--- app.js
+++ app.js
@@ -19,10 +19,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/movies',require('./routes/movie'));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/movies',require('./routes/movie'));
-app.use('/detail',require('./routes/detail'));
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -14,5 +14,47 @@ router.get('/deals', function(req, res, next) {
  res.render('deals.hbs', { title: 'Ưu đãi' });  
 });
 
+router.get('/:id', function (req, res, next) {
+    console.log(+req.params.id);
+  if (+req.params.id !== NaN) {
+      const row = {};
+      if(+req.params.id === 1){
+        row.id = 1;
+        row.ten = 'Mắt biếc';
+        row.thoiluong = 121;
+        row.mota = 'Mắt Biếc: Một sự kết hợp tinh tế vẻ đẹp thuần khiết của văn chương Nguyễn Nhật Ánh, với những khung hình mãn nhãn đặc trưng của Victor Vũ, đã từng khiến khán giả choáng ngợp từ “Thiên mệnh anh hùng” tới “Tôi thấy hoa vàng trên cỏ xanh”.'
+        row.nam = 2019;
+        row.theloai = 'Tình cảm';
+        row.daodien = 'Victor Vũ';
+        row.trailer = 'https://www.youtube.com/embed/ITlQ0oU7tDA';
+      }
 
+      if(+req.params.id === 2){
+        row.id = 2;
+        row.ten = 'Trẻ trâu khởi nghiệp';
+        row.thoiluong = 102;
+        row.mota = 'Những tưởng đường đời sẽ dễ thở hơn trường học, đôi bạn Taek-Il (Park Jung-Min) và Sang-Pil (Jung Hae-In) quyết nghỉ học để dấn thân vào đời. Taek-il xin làm shipper cho tiệm mì của đầu bếp Ma Dong-seok “cục súc", còn Sang-Pil đi theo bọn cho vay nặng lãi đòi nợ thuê. Từ đây hai thanh niên "trẻ trâu" mới có cơ hội nếm vị trưởng thành sau những lần bị đời vùi dập.'
+        row.nam = 2019;
+        row.theloai = 'Hài hước';
+        row.daodien = 'Choi Jung-yol';
+        row.trailer = 'https://www.youtube.com/embed/9SsehGW1Yb0';
+      }
+
+      if(+req.params.id === 3){
+        row.id = 3;
+        row.ten = 'Điệp viên ẩn danh';
+        row.thoiluong = 101;
+        row.mota = 'Biệt đội giải cứu thế giới của siêu điệp viên Lance Sterling và các phụ tá siêu tài năng và thông minh!'
+        row.nam = 2019;
+        row.theloai = 'Hài hước';
+        row.daodien = 'Troy Quane, Nick Bruno';
+        row.trailer = 'https://www.youtube.com/embed/E2zQAii4H8k';
+      }
+
+      res.render('single-film-review.hbs', {
+          title: 'Review',
+          movie: row,
+      });
+  }    
+});
 module.exports = router;
