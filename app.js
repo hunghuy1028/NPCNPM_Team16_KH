38ffe87c3a7579d39d870dc4ea749581fc426f38
thkduy <56596480+thkduy@users.noreply.github.com>
2019-12-31 11:20:18 +0700

    thêm đường dẫn vào app.js

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/movies',require('./routes/movie'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/movie.js
+++ routes/movie.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/listfilm', function (req, res, next) {
+router.get('/', function (req, res, next) {
     res.render('listfilm.hbs', { title: 'Danh sách phim' });
 });
 
